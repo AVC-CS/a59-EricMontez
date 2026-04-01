8a585809b943cd1916de3442a7aff9ca13721198
EricMontez <eric.alexander.montez@gmail.com>
2026-04-01 16:38:10 -0700

    A59 Done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int prev, current;
-	// TODO
+  // Initialize variables
+  int cur, prev;
 
-	// TODO
+  // Setup first iteration
+  cin >> cur;
+  prev = cur;
+
+  // Repeat until cur is greater than prev
+  while (1)
+  {
+    if (cur <= prev)
+    {
+      cout << cur << "  ";
+      prev = cur;
+      cin >> cur;
+    }
+    else
+     break;
+  }
+
+  // Print newline and end program
+  cout << endl;
+  return 0;
 }
